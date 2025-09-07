d30340823693436dc4bd76ff6bf7611c8c7315e3
pixelupmrk <pixelupmrkt@gmail.com>
2025-09-07 19:58:15 -0300

    Add files via upload

---
+++ api/wa/start.js
@@ -0,0 +1,7 @@
+
+const { allowCors, fwd } = require("./_common");
+
+module.exports = allowCors(async function handler(req, res) {
+  const r = await fwd("/api/wa/start", req);
+  res.status(r.status).send(r.data);
+});
